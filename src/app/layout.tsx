5a6ed08068f55f533ae7f6568bf4c7da6710e80c
Duck1405 <rd105291@outlook.com>
2025-08-02 20:08:46 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { env } from '@/utils/env'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'WEB_NAME',
+  title: 'Slicehosting',
   description: 'Monitor your service uptime and incidents',
 }
 
